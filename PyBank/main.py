0a11f64f04f015545fbbb6740fccc3ce998c63f6
nickccarson <nickccarson@gmail.com>
2019-06-18 20:46:27 -0500

    updated PyBank Assignment- NOT COMPLETE

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,47 +1,62 @@
 import os
 import csv
+import sys
 
-
+sys.stdout = open('solutions_pybank.txt' , 'w' )
 
 #budget_info_csv = 'budget_data.csv'
 
-budget_info_csv = os.path.join('budget_data.csv')
+#budget_info_csv = os.path.join('budget_data.csv')
 
-months =[]
-revenue = []
-
-
-with open(budget_info_csv,  newline='') as csvfile:
+with open("budget_data.csv",  newline='') as csvfile:
     csvreader = csv.reader(csvfile)
-    
-
-    for row in csvreader:
-        months.append(row[0])
-        revenue.append(int(row[1]))
-
-
-
-#with open(budget_info_csv,') as budget_data:
- #   csvreader = csv.reader(budget_data, delimiter=',')
-
-  #  print(csvreader)
-
-
-
-    # Read the header row first (skip this step if there is now header)
+    print(csvreader)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    print(csv_header)
 
 
+    csvreader_aslist =[]
+    csvreader_aslist= list(csvreader)
 
-for row in csvreader:
-        #print(row)
-        months.append(row[0])
+    #print(csvreader_aslist)
 
+    months=[]
+    months = len(csvreader_aslist)
+    print(months)
 
-print(months)
+    print("Financial Analysis")
+    print("----------------------------------------------------")
+    print("Total number of months " + str(months))
 
+    second_column =[]
+    second_column = [row[1] for row in csvreader_aslist]
+    #print(second_column)
 
-#budget_info_df = pd.read_csv(budget_info_csv)
-#budget_info_df.describe()
+    second_col_asint = []
+    second_col_asint= list(map(int,second_column))
+    #print(second_col_asint)
 
+    total_sum = sum(second_col_asint)
+    #print(total_sum)
+
+    #print(second_col_asint)
+    monthly_change=[]
+    monthly_change = [second_col_asint[i]-second_col_asint[i+1] for i in  range(len(second_col_asint)-1)]
+    #print(monthly_change)
+   
+    monthly_avg= sum(monthly_change)/(months-1)
+    print("Average Monthly Change" + str(monthly_avg))
+
+    max_val= max(second_col_asint)
+    #print(max_val)
+    print(second_col_asint.index(max_val))
+    max_val_month = second_col_asint.index(max_val)
+    print('Greatest Profit ' + str(csvreader_aslist[max_val_month]))
+
+    min_val= min(second_col_asint)
+    #print(min_val)
+    print(second_col_asint.index(min_val))
+    min_val_month = second_col_asint.index(min_val)    
+    print('Greatest Loss ' + str(csvreader_aslist[min_val_month]))
+
+    
